bac5eb45e89db9d27f87622ceb5c2449f08bcdc9
vemerion <vemerion@gmail.com>
2021-01-25 22:21:53 +0100

    Work on blood fluid
    This commit adds the water tag to the blood fluid, to get the water
    physics etc. Also adds a couple of client-side fog events to change the
    blood fog from blue (due to the water tag) to red. Also tweaks some of
    the blood fluid attributes to be more slow-flowing.

---
+++ src/main/java/mod/vemerion/runeworld/event/ClientForgeEventSubscriber.java
@@ -0,0 +1,30 @@
+package mod.vemerion.runeworld.event;
+
+import mod.vemerion.runeworld.Main;
+import mod.vemerion.runeworld.init.ModFluids;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityViewRenderEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+
+@EventBusSubscriber(bus = Bus.FORGE, modid = Main.MODID, value = Dist.CLIENT)
+public class ClientForgeEventSubscriber {
+
+	@SubscribeEvent
+	public static void redBloodFog(EntityViewRenderEvent.FogColors event) {
+		if (event.getInfo().getFluidState().getFluid().isEquivalentTo(ModFluids.BLOOD)) {
+			event.setBlue(0.15f);
+			event.setGreen(0.15f);
+			event.setRed(0.7f);
+		}
+	}
+	
+	@SubscribeEvent
+	public static void redBloodFog(EntityViewRenderEvent.FogDensity event) {
+		if (event.getInfo().getFluidState().getFluid().isEquivalentTo(ModFluids.BLOOD)) {
+			event.setDensity(0.35f);
+			event.setCanceled(true);
+		}
+	}
+}
--- src/main/java/mod/vemerion/runeworld/fluid/BloodFluid.java
+++ src/main/java/mod/vemerion/runeworld/fluid/BloodFluid.java
@@ -5,22 +5,24 @@ import mod.vemerion.runeworld.helpers.Helper;
 import mod.vemerion.runeworld.init.ModBlocks;
 import mod.vemerion.runeworld.init.ModFluids;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvents;
 import net.minecraftforge.fluids.ForgeFlowingFluid;
 
 // TODO: Blood bucket
-// TODO: Maybe add water tag to blood, to add water physics etc?
 public class BloodFluid {
 	private static final ForgeFlowingFluid.Properties properties = new ForgeFlowingFluid.Properties(
 			() -> ModFluids.BLOOD, () -> ModFluids.BLOOD_FLOWING,
-			net.minecraftforge.fluids.FluidAttributes.builder(
-					new ResourceLocation("block/water_still"),
-					new ResourceLocation("block/water_flow"))
+			net.minecraftforge.fluids.FluidAttributes
+					.builder(new ResourceLocation("block/water_still"), new ResourceLocation("block/water_flow"))
+					.sound(SoundEvents.ITEM_BUCKET_FILL, SoundEvents.ITEM_BUCKET_EMPTY)
 					.color(Helper.color(255, 0, 0, 255))
-					.translationKey("block." + Main.MODID + "blood")).block(() -> ModBlocks.BLOOD)
-							.slopeFindDistance(4).levelDecreasePerBlock(1).explosionResistance(100F);
-	
+					.overlay(new ResourceLocation(Main.MODID, "block/blood_overlay"))
+					.translationKey("block." + Main.MODID + "blood")).block(() -> ModBlocks.BLOOD).slopeFindDistance(4)
+							.levelDecreasePerBlock(1).explosionResistance(100F).tickRate(15).slopeFindDistance(3)
+							.levelDecreasePerBlock(2);
+
 	public static class Flowing extends ForgeFlowingFluid.Flowing {
-		
+
 		public Flowing() {
 			super(properties);
 		}
