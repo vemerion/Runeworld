1787fe4f04aaecf0e419109d4db124000f5f2485
vemerion <vemerion@gmail.com>
2021-03-02 22:20:17 +0100

    Implement blood dislocator functionality

--- src/main/java/mod/vemerion/runeworld/item/BloodDislocatorItem.java
+++ src/main/java/mod/vemerion/runeworld/item/BloodDislocatorItem.java
@@ -1,11 +1,49 @@
 package mod.vemerion.runeworld.item;
 
+import mod.vemerion.runeworld.capability.RuneTeleport;
+import mod.vemerion.runeworld.init.ModDimensions;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.UseAction;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
 
+// TODO: Add First-person use animation
 public class BloodDislocatorItem extends Item {
 
 	public BloodDislocatorItem() {
 		super(new Item.Properties().group(ItemGroup.SEARCH));
+	}
+
+	@Override
+	public int getUseDuration(ItemStack stack) {
+		return 30;
+	}
+
+	@Override
+	public UseAction getUseAction(ItemStack stack) {
+		return UseAction.BLOCK;
+	}
+
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
+		ItemStack stack = playerIn.getHeldItem(handIn);
+		playerIn.setActiveHand(handIn);
+		return ActionResult.resultConsume(stack);
+	}
+
+	@Override
+	public ItemStack onItemUseFinish(ItemStack stack, World worldIn, LivingEntity entityLiving) {
+		if (entityLiving instanceof PlayerEntity && !((PlayerEntity) entityLiving).isCreative())
+			stack.shrink(1);
+
+		if (!worldIn.isRemote)
+			RuneTeleport.teleport(entityLiving, worldIn, ModDimensions.BLOOD);
+
+		return stack;
 	}
 }
