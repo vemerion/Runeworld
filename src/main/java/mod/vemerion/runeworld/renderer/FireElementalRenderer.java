d582432b1aa9fc823d6dba0652c4612f9011f78a
vemerion <vemerion@gmail.com>
2021-03-07 10:37:49 +0100

    Shaky fire elemental body

--- src/main/java/mod/vemerion/runeworld/renderer/FireElementalRenderer.java
+++ src/main/java/mod/vemerion/runeworld/renderer/FireElementalRenderer.java
@@ -1,17 +1,24 @@
 package mod.vemerion.runeworld.renderer;
 
+import java.util.Random;
+
 import com.mojang.blaze3d.matrix.MatrixStack;
 
 import mod.vemerion.runeworld.Main;
 import mod.vemerion.runeworld.entity.FireElementalEntity;
+import mod.vemerion.runeworld.helpers.Helper;
 import mod.vemerion.runeworld.init.ModBlocks;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.BlockRendererDispatcher;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.util.Direction;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.client.model.data.EmptyModelData;
 
 public class FireElementalRenderer extends EntityRenderer<FireElementalEntity> {
@@ -29,9 +36,45 @@ public class FireElementalRenderer extends EntityRenderer<FireElementalEntity> {
 			IRenderTypeBuffer bufferIn, int packedLightIn) {
 		super.render(entityIn, entityYaw, partialTicks, matrixStackIn, bufferIn, packedLightIn);
 
+		int ticksExisted = entityIn.ticksExisted;
+		float ageInTicks = ticksExisted + partialTicks;
+		Random rand = new Random(2);
+
 		Minecraft mc = Minecraft.getInstance();
-		mc.getBlockRendererDispatcher().renderBlock(BODY, matrixStackIn, bufferIn, packedLightIn,
-				OverlayTexture.NO_OVERLAY, EmptyModelData.INSTANCE);
+		BlockRendererDispatcher blockRenderer = mc.getBlockRendererDispatcher();
+
+		matrixStackIn.push();
+		matrixStackIn.translate(0.5, 1, -0.5);
+
+		// body
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				renderPart(matrixStackIn, bufferIn, packedLightIn, ageInTicks, rand, blockRenderer);
+				BlockPos direction = BlockPos.ZERO.offset(Direction.byHorizontalIndex(j));
+				matrixStackIn.translate(direction.getX(), 0, direction.getZ());
+			}
+			matrixStackIn.translate(0, 1, 0);
+		}
+
+		matrixStackIn.pop();
+	}
+
+	private void renderPart(MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, int packedLightIn, float ageInTicks,
+			Random rand, BlockRendererDispatcher blockRenderer) {
+		matrixStackIn.push();
+		matrixStackIn.translate(-0.5, -0.5, -0.5);
+		shake(matrixStackIn, rand, ageInTicks);
+		blockRenderer.renderBlock(BODY, matrixStackIn, bufferIn, packedLightIn, OverlayTexture.NO_OVERLAY,
+				EmptyModelData.INSTANCE);
+		matrixStackIn.pop();
+	}
+
+	private void shake(MatrixStack matrix, Random rand, float ageInTicks) {
+		matrix.translate(randPos(rand, ageInTicks), randPos(rand, ageInTicks), randPos(rand, ageInTicks));
+	}
+
+	private double randPos(Random rand, float ageInTicks) {
+		return MathHelper.cos(ageInTicks + Helper.toRad(rand.nextDouble() * 360)) * 0.01;
 	}
 
 	@Override
