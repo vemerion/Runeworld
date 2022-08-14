5ebcce34b311ab7aab9d8fd26e84a7ea365f5aa8
vemerion <vemerion@gmail.com>
2022-08-14 10:27:02 +0200

    Add blocks to mineable tags

--- src/main/java/mod/vemerion/runeworld/datagen/ModBlockTagsProvider.java
+++ src/main/java/mod/vemerion/runeworld/datagen/ModBlockTagsProvider.java
@@ -1,9 +1,10 @@
 package mod.vemerion.runeworld.datagen;
 
 import mod.vemerion.runeworld.Main;
+import mod.vemerion.runeworld.block.complex.StoneMaterial;
 import mod.vemerion.runeworld.init.ModBlocks;
-import net.minecraft.data.tags.BlockTagsProvider;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.data.tags.BlockTagsProvider;
 import net.minecraft.tags.BlockTags;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
@@ -16,8 +17,18 @@ public class ModBlockTagsProvider extends BlockTagsProvider {
 
 	@Override
 	protected void addTags() {
-		tag(BlockTags.WALLS).add(ModBlocks.SPARKSTONE.WALL);
-		tag(BlockTags.WALLS).add(ModBlocks.CHARRED_STONE.WALL);
+		tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModBlocks.BLOOD_PILLAR_LARGE, ModBlocks.BLOOD_PILLAR_MEDIUM, ModBlocks.BLOOD_PILLAR_SMALL, ModBlocks.BLOOD_MOSS, ModBlocks.BLOOD_ROCK, ModBlocks.HIDEABLE_BLOOD_ROCK, ModBlocks.FIRE_RITUAL_STONE);
+		tag(BlockTags.MINEABLE_WITH_SHOVEL).add(ModBlocks.CHARRED_DIRT, ModBlocks.BURNT_DIRT);
+		
+		stoneMaterial(ModBlocks.CHARRED_STONE);
+		stoneMaterial(ModBlocks.SPARKSTONE);
+	}
+	
+	private void stoneMaterial(StoneMaterial material) {
+		tag(BlockTags.WALLS).add(material.WALL);
+		for (var block : material.getBlocks()) {
+			tag(BlockTags.MINEABLE_WITH_PICKAXE).add(block);
+		}
 	}
 
 }
